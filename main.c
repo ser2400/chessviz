a56cbfaa3e62ce8fb18d3701f30a57f180e8dfab
ser2400 <ser2400@gmail.com>
2018-05-22 18:41:33 +0700

    created main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("\
+	8 R K B K Q B K R\n\
+	7 P P P P P P P P\n\
+	6\n\
+	5\n\
+	4\n\
+	3\n\
+	2 P P P P P P P P\n\
+	1 R K B K Q B K R\n\
+	  a b c d e f g h");
+	return 0;
+}
